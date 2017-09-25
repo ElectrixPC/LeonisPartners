ec75a16a4bbad7a0c7beebab482ab1977d4dc538
James Allington-Kay <lancaster.1@hotmail.co.uk>
2017-09-25 12:22:19 +0100

    Initial Commit
    First wireframe commit

---
+++ leonisWPTheme/index.php
@@ -0,0 +1,5 @@
+<link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700" rel="stylesheet">
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+<title>Leonis Partners</title>
+<h1>Sup Fuckface</h1>
+<h2>Let's do this</h2>
