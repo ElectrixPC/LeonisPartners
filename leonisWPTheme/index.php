8dae8667f913767ff75ffc9252bb2c5c74017923
James Allington-Kay <lancaster.1@hotmail.co.uk>
2017-09-25 22:26:15 +0100

    Fixing merge conflict between server and local

--- leonisWPTheme/index.php
+++ leonisWPTheme/index.php
@@ -1,22 +1,24 @@
 <!DOCTYPE html>
-<html >
+<html>
 <head>
     <meta charset="UTF-8">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700" rel="stylesheet">
-    <link rel="stylesheet" href="style.css"><!--<?php bloginfo('stylesheet_url'); ?>">-->
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
     <title>Leonis Partners</title>
 </head>
 
 <body>
+  <img style="height: 50px;position: relative;top: 50px;display: block;margin: 0 auto;width: auto;" src="<?php echo get_bloginfo('template_url') ?>/images/white_logo_text_small-1.png"/>
   <div class="parallax">
     <div class="parallax__layer parallax__layer__0">
-        <img src="nyc-back.svg" />
+        <img src="<?php echo get_bloginfo('template_url') ?>/images/nyc-back.svg"/>
     </div>
     <div class="parallax__layer parallax__layer__1">
-        <img src="nyc-middle.svg" />
+            <img src="<?php echo get_bloginfo('template_url') ?>/images/nyc-middle.svg"/>
     </div>
     <div class="parallax__layer parallax__layer__2">
-        <img src="nyc-front.svg" />
+            <img src="<?php echo get_bloginfo('template_url') ?>/images/nyc-front.svg"/>
+    </div>
 
     <div class="parallax__cover"></div>
 </div>
