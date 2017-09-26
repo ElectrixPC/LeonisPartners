04b22e179d9ebeac3eebddf026b5b70f8b3e035e
James Allington-Kay <lancaster.1@hotmail.co.uk>
2017-09-26 16:03:17 +0100

    Adding basic wireframe structure

--- leonisWPTheme/index.php
+++ leonisWPTheme/index.php
@@ -1,29 +1,15 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700" rel="stylesheet">
-    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
-    <title>Leonis Partners</title>
-</head>
-
-<body>
-  <img style="height: 50px;position: relative;top: 50px;display: block;margin: 0 auto;width: auto;" src="<?php echo get_bloginfo('template_url') ?>/images/white_logo_text_small-1.png"/>
-  <div class="parallax">
-    <div class="parallax__layer parallax__layer__0">
-        <img src="<?php echo get_bloginfo('template_url') ?>/images/nyc-back.svg"/>
-    </div>
-    <div class="parallax__layer parallax__layer__1">
-            <img src="<?php echo get_bloginfo('template_url') ?>/images/nyc-middle.svg"/>
-    </div>
-    <div class="parallax__layer parallax__layer__2">
-            <img src="<?php echo get_bloginfo('template_url') ?>/images/nyc-front.svg"/>
-    </div>
-
-    <div class="parallax__cover"></div>
+<?php get_header(); ?>
+<div id="main">
+<div id="content">
+<h1>Main Area</h1>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<h1><?php the_title(); ?></h1>
+<h4>Posted on <?php the_time('F jS, Y') ?></h4>
+<p><?php the_content(__('(more...)')); ?></p>
+<hr> <?php endwhile; else: ?>
+<p><?php _e('Sorry, no posts matched your criteria.'); ?></p><?php endif; ?>
 </div>
-  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-
-  
-</body>
-</html>
+</div>
+<div id="delimiter">
+</div>
+<?php get_footer(); ?>
