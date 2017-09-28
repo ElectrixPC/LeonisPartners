dc3eb7e876575b3933a391a037cac415194bd9ac
James Allington-Kay <lancaster.1@hotmail.co.uk>
2017-09-28 19:40:17 +0100

    Amending get all posts function
    Whenever you add a page, itll addd it to the home page.

--- leonisWPTheme/home.php
+++ leonisWPTheme/home.php
@@ -20,12 +20,16 @@
 
     <div id="primary" class="site-content">
         <div id="content" role="main">
-            <?php
-                $args = array("post_type" => "page", "order" => "ASC", "orderby" => "menu_order");
-                $the_query = new WP_Query($args);
-            ?>
-            <?php if(have_posts()):while($the_query->have_posts()):$the_query->the_post(); ?>
-            <?php get_template_part("content", "page"); ?>
+            <?php query_posts('post_type=page'); ?>
+
+            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+            <?php the_title(); ?>
+
+
+
+            <?php the_content(); ?>
+
             <?php endwhile; endif; ?>
         </div><!-- #content -->
     </div><!-- #primary -->
