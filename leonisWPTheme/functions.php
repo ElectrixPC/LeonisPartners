8dc878559aba04de57e847611e52086fdef479bb
James Allington-Kay <lancaster.1@hotmail.co.uk>
2017-11-04 13:02:54 +0000

    Adding transactions shortcode for use in the page
    Creating a basic output function

--- leonisWPTheme/functions.php
+++ leonisWPTheme/functions.php
@@ -34,9 +34,7 @@ function transactions_init() {
         'menu_icon' => 'dashicons-chart-line',
         'supports' => array(
             'title',
-            'revisions',
             'thumbnail',
-            'author',
             'page-attributes',)
         );
     register_post_type( 'transactions', $args );
@@ -44,3 +42,17 @@ function transactions_init() {
 add_action( 'init', 'transactions_init' );
 
 add_theme_support( 'post-thumbnails' );
+
+// [transactions]
+function transactions_output() {
+    $args = array(
+        'post_type' => 'transactions'
+    ); // these arguments are telling WP_Query to only look for the post types called transactions
+    $query = new WP_Query( $args );
+
+    while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+        <h2><?php the_title(); ?></h2>
+        the_post_thumbnail();
+    endwhile;
+}
+add_shortcode( 'transactions', 'transactions_output()');
