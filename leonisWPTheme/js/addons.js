fc3aba4b2b96ece1beed0ce1ac47670ab759550b
James Allington-Kay <lancaster.1@hotmail.co.uk>
2017-12-04 13:35:02 +0000

    Adding more styling to the download file

--- leonisWPTheme/js/addons.js
+++ leonisWPTheme/js/addons.js
@@ -335,5 +335,6 @@ window.addEventListener('resize', function(event){
 });
 
 $('.button-file').click(function() {
-    $('.wpcf7').css({"display":"block", "height":"50%", "width":"50%"});
+    $('.wpcf7').css({"height":"50%", "width":"50%"});
+    $('.wpcf7-form').css({"display" : "block"});
 });
