032d4c8c97f86955c1e414140ca0f3f34e6fa71a
James Allington-Kay <lancaster.1@hotmail.co.uk>
2017-12-06 14:03:17 +0000

    removing no height

--- leonisWPTheme/js/addons.js
+++ leonisWPTheme/js/addons.js
@@ -353,26 +353,26 @@ $('.wpcf7-submit').click(function() {
 });
 
 $('.box-ma').click(function() {
-    $('.services-cr').css({"opacity":"0", "position":"absolute", "height": "0px"});
-    $('.services-fv').css({"opacity":"0", "position":"absolute", "height": "0px"});
-    $('.services-ss').css({"opacity":"0", "position":"absolute", "height": "0px"});
-    $('.services-ma').css({"opacity":"1", "position":"relative", "height": "auto"});
+    $('.services-cr').css({"opacity":"0", "position":"absolute"});
+    $('.services-fv').css({"opacity":"0", "position":"absolute"});
+    $('.services-ss').css({"opacity":"0", "position":"absolute"});
+    $('.services-ma').css({"opacity":"1", "position":"relative"});
 });
 $('.box-cr').click(function() {
-    $('.services-fv').css({"opacity":"0", "position":"absolute", "height": "0px"});
-    $('.services-ss').css({"opacity":"0", "position":"absolute", "height": "0px"});
-    $('.services-ma').css({"opacity":"0", "position":"absolute", "height": "0px"});
-    $('.services-cr').css({"opacity":"1", "position":"relative", "height": "auto"});
+    $('.services-fv').css({"opacity":"0", "position":"absolute"});
+    $('.services-ss').css({"opacity":"0", "position":"absolute"});
+    $('.services-ma').css({"opacity":"0", "position":"absolute"});
+    $('.services-cr').css({"opacity":"1", "position":"relative"});
 });
 $('.box-fv').click(function() {
-    $('.services-cr').css({"opacity":"0", "position":"absolute", "height": "0px"});
-    $('.services-ss').css({"opacity":"0", "position":"absolute", "height": "0px"});
-    $('.services-ma').css({"opacity":"0", "position":"absolute", "height": "0px"});
-    $('.services-fv').css({"opacity":"1", "position":"relative", "height": "auto"});
+    $('.services-cr').css({"opacity":"0", "position":"absolute"});
+    $('.services-ss').css({"opacity":"0", "position":"absolute"});
+    $('.services-ma').css({"opacity":"0", "position":"absolute"});
+    $('.services-fv').css({"opacity":"1", "position":"relative"});
 });
 $('.box-ss').click(function() {
-    $('.services-cr').css({"opacity":"0", "position":"absolute", "height": "0px"});
-    $('.services-ma').css({"opacity":"0", "position":"absolute", "height": "0px"});
-    $('.services-fv').css({"opacity":"0", "position":"absolute", "height": "0px"});
-    $('.services-ss').css({"opacity":"1", "position":"relative", "height": "auto"});
+    $('.services-cr').css({"opacity":"0", "position":"absolute"});
+    $('.services-ma').css({"opacity":"0", "position":"absolute"});
+    $('.services-fv').css({"opacity":"0", "position":"absolute"});
+    $('.services-ss').css({"opacity":"1", "position":"relative"});
 });
