ba6771d875eec82860db03ad170b37961faeefdc
James Allington-Kay <lancaster.1@hotmail.co.uk>
2017-12-06 01:34:31 +0000

    fixing multiple transactions

--- leonisWPTheme/functions.php
+++ leonisWPTheme/functions.php
@@ -93,7 +93,7 @@ function transactions_output($atts) {
           $firm2_img  = '<div class="trans-img2"><img src=' . get_post_meta(get_the_ID(), "wpcf-company-2",                 true) . '></div>';
           $type_short = '<div class="trans-type-short"><p><b>Type:</b> ' . get_post_meta(get_the_ID(), "wpcf-transaction-type-short",    true) . '</p></div>';
           $sector     = '<div class="trans-sector"><p><b>Sector:</b> '     . get_post_meta(get_the_ID(), "wpcf-transaction-sector",        true) . '</p></div>';
-          $location   = '<div class="trans-location"><p><b>Company Location: </b></p><div class="trans-location-content"><p>' . get_post_meta($postid, "wpcf-transaction-location", true) . '</p></div></div>';
+          $location   = '<div class="trans-location"><p><b>Company Location: </b></p><div class="trans-location-content"><p>' . get_post_meta(get_the_ID(), "wpcf-transaction-location", true) . '</p></div></div>';
           if ($count>$atts["posts"]) {
             // if there are more than 10 posts hide them
             $trans = '<div class="transaction trans_hide">';
