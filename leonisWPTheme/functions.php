0862c697297e7ae1c6b86784c8f0090985cea350
James Allington-Kay <lancaster.1@hotmail.co.uk>
2017-12-18 11:55:52 +0000

    Changing name to news from press

--- leonisWPTheme/functions.php
+++ leonisWPTheme/functions.php
@@ -55,27 +55,27 @@ add_action( 'init', 'transactions_init' );
 function revcon_change_post_label() {
     global $menu;
     global $submenu;
-    $menu[5][0] = 'Press';
-    $submenu['edit.php'][5][0] = 'Press';
-    $submenu['edit.php'][10][0] = 'Add Press';
-    $submenu['edit.php'][16][0] = 'Press Tags';
+    $menu[5][0] = 'News';
+    $submenu['edit.php'][5][0] = 'News';
+    $submenu['edit.php'][10][0] = 'Add News';
+    $submenu['edit.php'][16][0] = 'News Tags';
 }
 function revcon_change_post_object() {
     global $wp_post_types;
     $labels = &$wp_post_types['post']->labels;
-    $labels->name = 'Press';
-    $labels->singular_name = 'Press';
-    $labels->add_new = 'Add Press';
-    $labels->add_new_item = 'Add Press';
-    $labels->edit_item = 'Edit Press';
-    $labels->new_item = 'Press';
-    $labels->view_item = 'View Press';
-    $labels->search_items = 'Search Press';
-    $labels->not_found = 'No Press found';
-    $labels->not_found_in_trash = 'No Press found in Trash';
-    $labels->all_items = 'All Press';
-    $labels->menu_name = 'Press';
-    $labels->name_admin_bar = 'Press';
+    $labels->name = 'News';
+    $labels->singular_name = 'News';
+    $labels->add_new = 'Add News';
+    $labels->add_new_item = 'Add News';
+    $labels->edit_item = 'Edit News';
+    $labels->new_item = 'News';
+    $labels->view_item = 'View News';
+    $labels->search_items = 'Search News';
+    $labels->not_found = 'No News found';
+    $labels->not_found_in_trash = 'No News found in Trash';
+    $labels->all_items = 'All News';
+    $labels->menu_name = 'News';
+    $labels->name_admin_bar = 'News';
 }
  
 add_action( 'admin_menu', 'revcon_change_post_label' );
