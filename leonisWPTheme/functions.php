9d3c0afbb0119f80000ca9b0f04a59c5dc6f7861
James Allington-Kay <lancaster.1@hotmail.co.uk>
2017-12-19 15:51:20 +0000

    Fixing php bug

--- leonisWPTheme/functions.php
+++ leonisWPTheme/functions.php
@@ -22,9 +22,12 @@ add_action( 'after_setup_theme', 'leonis_setup' );
 
 function leonis_all_scriptsandstyles() {
     //Load JS and CSS files in here
+      wp_register_script ('scrolling',  get_stylesheet_directory_uri() . '/js/jquery.scrollme.js', array( 'jquery'),'1',true);
       wp_register_script ('navigation', get_stylesheet_directory_uri() . '/js/addons.js', array( 'jquery' ),'1',true);
     
       wp_register_style ('scssfile', get_stylesheet_directory_uri() . '/scss/style.scss', array(),'2','all');
+      
+      wp_enqueue_script('scrolling');
       wp_enqueue_script('navigation');
       wp_enqueue_style( 'scssfile');
     
