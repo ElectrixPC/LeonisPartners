5a49a66a02fe076228aff4771a30ef8b0da85756
James Allington-Kay <lancaster.1@hotmail.co.uk>
2017-12-22 14:01:47 +0000

    Adding function for team

--- leonisWPTheme/functions.php
+++ leonisWPTheme/functions.php
@@ -25,13 +25,43 @@ function leonis_all_scriptsandstyles() {
       wp_register_script ('navigation', get_stylesheet_directory_uri() . '/js/addons.js', array( 'jquery' ),'1',true);
     
       wp_register_style ('scssfile', get_stylesheet_directory_uri() . '/scss/style.scss', array(),'2','all');
-      
-      wp_enqueue_script('scrolling');
+    
       wp_enqueue_script('navigation');
       wp_enqueue_style( 'scssfile');
     
     }
-    add_action( 'wp_enqueue_scripts', 'leonis_all_scriptsandstyles' );
+add_action( 'wp_enqueue_scripts', 'leonis_all_scriptsandstyles' );
+
+function revcon_change_post_label() {
+    global $menu;
+    global $submenu;
+    $menu[5][0] = 'Press';
+    $submenu['edit.php'][5][0] = 'Press';
+    $submenu['edit.php'][10][0] = 'Add Press';
+    $submenu['edit.php'][16][0] = 'Press Tags';
+}
+function revcon_change_post_object() {
+    global $wp_post_types;
+    $labels = &$wp_post_types['post']->labels;
+    $labels->name = 'Press';
+    $labels->singular_name = 'Press';
+    $labels->add_new = 'Add Press';
+    $labels->add_new_item = 'Add Press';
+    $labels->edit_item = 'Edit Press';
+    $labels->new_item = 'Press';
+    $labels->view_item = 'View Press';
+    $labels->search_items = 'Search Press';
+    $labels->not_found = 'No Press found';
+    $labels->not_found_in_trash = 'No Press found in Trash';
+    $labels->all_items = 'All Press';
+    $labels->menu_name = 'Press';
+    $labels->name_admin_bar = 'Press';
+}
+ 
+add_action( 'admin_menu', 'revcon_change_post_label' );
+add_action( 'init', 'revcon_change_post_object' );
+
+
 
 // Creates the transactions section in the wp editor
 function transactions_init() {
@@ -52,35 +82,25 @@ function transactions_init() {
 }
 add_action( 'init', 'transactions_init' );
 
+// Creates the team section in the wp editor
+function team_init() {
+    $args = array(
+      'label' => 'Team',
+        'public' => true,
+        'show_ui' => true,
+        'capability_type' => 'post',
+        'hierarchical' => false,
+        'rewrite' => array('slug' => 'team'),
+        'query_var' => true,
+        'menu_icon' => 'dashicons-businessman',
+        'supports' => array(
+            'title',
+            'page-attributes',)
+        );
+    register_post_type( 'team', $args );
+}
+add_action( 'init', 'team_init' );
 
-function revcon_change_post_label() {
-    global $menu;
-    global $submenu;
-    $menu[5][0] = 'News';
-    $submenu['edit.php'][5][0] = 'News';
-    $submenu['edit.php'][10][0] = 'Add News';
-    $submenu['edit.php'][16][0] = 'News Tags';
-}
-function revcon_change_post_object() {
-    global $wp_post_types;
-    $labels = &$wp_post_types['post']->labels;
-    $labels->name = 'News';
-    $labels->singular_name = 'News';
-    $labels->add_new = 'Add News';
-    $labels->add_new_item = 'Add News';
-    $labels->edit_item = 'Edit News';
-    $labels->new_item = 'News';
-    $labels->view_item = 'View News';
-    $labels->search_items = 'Search News';
-    $labels->not_found = 'No News found';
-    $labels->not_found_in_trash = 'No News found in Trash';
-    $labels->all_items = 'All News';
-    $labels->menu_name = 'News';
-    $labels->name_admin_bar = 'News';
-}
- 
-add_action( 'admin_menu', 'revcon_change_post_label' );
-add_action( 'init', 'revcon_change_post_object' );
 
 
 // [transactions] - main function for the transactions UI, points to the shortcode [transactions]
@@ -99,7 +119,7 @@ function transactions_output($atts) {
           $firm2      = '<div class="trans-firm2"><h2>'     . get_post_meta(get_the_ID(), "wpcf-company-2-name",            true) . '</h2></div>';
           $type       = '<div class="trans-type"><p>'       . get_post_meta(get_the_ID(), "wpcf-transaction-type",          true) . '</p></div>';
           $size       = '<div class="trans-size"><p><b>Transaction Size:</b> ' . get_post_meta(get_the_ID(), "wpcf-transaction-size",          true) . '</p></div>'; 
-          $press      = '<div class="trans-press"></div>'; //<p><b>Press Release:</b> '   . get_post_meta(get_the_ID(), "wpcf-transaction-press-release", true) . '</p></div>';
+          $press      = '<div class="trans-press"><p><b>Press Release:</b> '   . get_post_meta(get_the_ID(), "wpcf-transaction-press-release", true) . '</p></div>';
           $date       = '<div class="trans-date"><p><b>Date:</b> ' . get_post_meta(get_the_ID(), "wpcf-transaction-date",          true) . '</p></div>';
 
           if (get_post_meta(get_the_ID(), "wpcf-company-1", true)) {
@@ -119,12 +139,19 @@ function transactions_output($atts) {
           $sector     = '<div class="trans-sector"><p><b>Sector:</b> '     . get_post_meta(get_the_ID(), "wpcf-transaction-sector",        true) . '</p></div>';
           $location   = '<div class="trans-location"><p><b>Company Location: </b></p><div class="trans-location-content"><p>' . get_post_meta(get_the_ID(), "wpcf-transaction-location", true) . '</p></div></div>';
           $exit       = '<div class="trans-exit"><i class="fa fa-times fa-2x" aria-hidden="true"></i></div>';
-          $trans      = '<div class="transaction trans_hide">';
+          if ($count>$atts["posts"]) {
+            // if there are more than 10 posts hide them
+            $trans = '<div class="transaction trans_hide">';
+          }
+          else
+          {
+            $trans = '<div class="transaction">';
+          }
           $output .= $trans . '<div class="trans-expand">' . $firm1_img . $type . $firm2_img . $title . $firm1 . $firm2 . $size . $date . $location . $sector . $type_short . $press . $exit . '</div></div>';
           $count++;
     endwhile;
 
-    $output .= '</div><a type="button" class="button button-trans" href="javascript:void(0);">View All Transactions</a>';
+    $output .= '</div>';
     
     return $output; 
 }
@@ -141,7 +168,7 @@ function single_transaction($atts) {
     $firm2      = '<div class="trans-firm2"><h2>'     . get_post_meta($postid, "wpcf-company-2-name",            true) . '</h2></div>';
     $type       = '<div class="trans-type"><p>'       . get_post_meta($postid, "wpcf-transaction-type",          true) . '</p></div>';
     $size       = '<div class="trans-size"><p><b>Transaction Size:</b> ' . get_post_meta($postid, "wpcf-transaction-size",          true) . '</p></div>'; 
-    $press      = '<div class="trans-press"></div'; //<p><b>Press Release:</b> '   . get_post_meta($postid, "wpcf-transaction-press-release", true) . '</p></div>';
+    $press      = '<div class="trans-press"><p><b>Press Release:</b> '   . get_post_meta($postid, "wpcf-transaction-press-release", true) . '</p></div>';
     $date       = '<div class="trans-date"><p><b>Date:</b> ' . get_post_meta($postid, "wpcf-transaction-date",          true) . '</p></div>';
     if (get_post_meta($postid, "wpcf-company-1", true)) {
         $firm1_img  = '<div class="trans-img1"><img src=' . get_post_meta($postid, "wpcf-company-1", true) . '></div>';
@@ -157,12 +184,6 @@ function single_transaction($atts) {
     }
     $type_short = '<div class="trans-type-short"><p><b>Type:</b> ' . get_post_meta($postid, "wpcf-transaction-type-short",    true) . '</p></div>';
     $sector     = '<div class="trans-sector"><p><b>Sector:</b> '   . get_post_meta($postid, "wpcf-transaction-sector",        true) . '</p></div>';
-    if (get_post_meta($postid, "wpcf-transaction-location", true)) {
-        $location   = '<div class="trans-location"><p><b>Company Location: </b></p><div class="trans-location-content"><p>' . get_post_meta($postid, "wpcf-transaction-location", true) . '</p></div></div>';
-    }
-    else {
-        $location   = '<div class="trans-location"></div>';
-    }
     $location   = '<div class="trans-location"><p><b>Company Location: </b></p><div class="trans-location-content"><p>' . get_post_meta($postid, "wpcf-transaction-location", true) . '</p></div></div>';
     $exit       = '<div class="trans-exit"><i class="fa fa-times fa-2x" aria-hidden="true"></i></div>';
     $trans = '<div class="transaction">';
@@ -176,7 +197,7 @@ add_shortcode( 'transaction-single', 'single_transaction');
 function download_by_email($atts) {
     $downloadid = $atts['id'];
 
-    $output = '<div class="email-download"><div class="email-download-title"><a type="button" class="button button-file" href="javascript:void(0);">Download File
+    $output = '<div class="email-download"><div class="email-download-title"><a type="button" class="button button-file" href="#">Download File
     </a></div>';
     
     $shortcode = sprintf('[email-download download_id="%1$s" contact_form_id="295"]', $downloadid);
@@ -189,7 +210,7 @@ add_shortcode('email', 'download_by_email');
 
 function get_news($atts) {
     $args = array('post_type' => 'post',
-    'posts_per_page' => 1000); // these arguments are telling WP_Query to only look for the post types called transactions
+    'posts_per_page' => 4); // these arguments are telling WP_Query to only look for the post types called transactions
     $query = new WP_Query( $args );
     $output = '<div class="news-container">';
 
@@ -211,7 +232,8 @@ add_shortcode( 'news', 'get_news');
 
 function get_social($atts) {
     
-    $output  = '<div class="news-social-container"><div class="social"><i class="fa fa-twitter-square fa-8x" aria-hidden="true"></i><br><a type="button" class="button button-social" href="javascript:void(0);">Follow Us</a></div><div class="social"><i class="fa fa-linkedin-square fa-8x" aria-hidden="true"></i><br><a type="button" class="button button-social" href="https://www.linkedin.com/in/derickschaudies/">Connect with Us</a></div></div>';
+    $output = '<div class="news-social-container"><a type="button" href="#"><i class="fa fa-twitter-square fa-8x" aria-hidden="true"></i></a>';
+    $output .= '<a type="button" href="#"><i class="fa fa-linkedin-square fa-8x" aria-hidden="true"></i></a></div>';
 
     return $output;
 }
@@ -222,35 +244,35 @@ function get_downloads($atts) {
     $output = '<br><div class="news-download-container">';
     if (empty($atts['id1']) == false) {
         $output .= '<div class="news-download-item"><i class="fa fa-download fa-5x" aria-hidden="true"></i><i class="fa fa-check fa-5x" aria-hidden="true"></i><h2>' . get_the_title($atts['id1']) . '</h2>';
-        $output .= '<div class="email-download"><div class="email-download-title"><a type="button" class="button button-file" href="javascript:void(0);">Download File</a></div>';
+        $output .= '<div class="email-download"><div class="email-download-title"><a type="button" class="button button-file" href="#">Download File</a></div>';
         $shortcode = sprintf('[email-download download_id="%1$s" contact_form_id="358"]', $atts['id1']);
         $output .= do_shortcode( $shortcode );
         $output .= '</div></div>';
     }
     if (empty($atts['id2']) == false) {
         $output .= '<div class="news-download-item"><i class="fa fa-download fa-5x" aria-hidden="true"></i><i class="fa fa-check fa-5x" aria-hidden="true"></i><h2>' . get_the_title($atts['id2']) . '</h2>';
-        $output .= '<div class="email-download"><div class="email-download-title"><a type="button" class="button button-file" href="javascript:void(0);">Download File</a></div>';
+        $output .= '<div class="email-download"><div class="email-download-title"><a type="button" class="button button-file" href="#">Download File</a></div>';
         $shortcode = sprintf('[email-download download_id="%1$s" contact_form_id="295"]', $atts['id2']);
         $output .= do_shortcode( $shortcode );
         $output .= '</div></div>';
     }
     if (empty($atts['id3']) == false) {
         $output .= '<div class="news-download-item"><i class="fa fa-download fa-5x" aria-hidden="true"></i><i class="fa fa-check fa-5x" aria-hidden="true"></i><h2>' . get_the_title($atts['id3']) . '</h2>';
-        $output .= '<div class="email-download"><div class="email-download-title"><a type="button" class="button button-file" href="javascript:void(0);">Download File</a></div>';
+        $output .= '<div class="email-download"><div class="email-download-title"><a type="button" class="button button-file" href="#">Download File</a></div>';
         $shortcode = sprintf('[email-download download_id="%1$s" contact_form_id="295"]', $atts['id3']);
         $output .= do_shortcode( $shortcode );
         $output .= '</div></div>';
     }
     if (empty($atts['id4']) == false) {
         $output .= '<div class="news-download-item"><i class="fa fa-download fa-5x" aria-hidden="true"></i><i class="fa fa-check fa-5x" aria-hidden="true"></i><h2>' . get_the_title($atts['id4']) . '</h2>';
-        $output .= '<div class="email-download"><div class="email-download-title"><a type="button" class="button button-file" href="javascript:void(0);">Download File</a></div>';
+        $output .= '<div class="email-download"><div class="email-download-title"><a type="button" class="button button-file" href="#">Download File</a></div>';
         $shortcode = sprintf('[email-download download_id="%1$s" contact_form_id="295"]', $atts['id4']);
         $output .= do_shortcode( $shortcode );
         $output .= '</div></div>';
     }
     if (empty($atts['id5']) == false) {
         $output .= '<div class="news-download-item"><i class="fa fa-download fa-5x" aria-hidden="true"></i><i class="fa fa-check fa-5x" aria-hidden="true"></i><h2>' . get_the_title($atts['id5']) . '</h2>';
-        $output .= '<div class="email-download"><div class="email-download-title"><a type="button" class="button button-file" href="javascript:void(0);">Download File</a></div>';
+        $output .= '<div class="email-download"><div class="email-download-title"><a type="button" class="button button-file" href="#">Download File</a></div>';
         $shortcode = sprintf('[email-download download_id="%1$s" contact_form_id="295"]', $atts['id5']);
         $output .= do_shortcode( $shortcode );
         $output .= '</div></div>';
