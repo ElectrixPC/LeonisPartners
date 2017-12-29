7870ab2b195896ebbe523c85828020aeea1c4062
James Allington-Kay <lancaster.1@hotmail.co.uk>
2017-12-29 14:57:57 +0000

    Fixing text for email and phone

--- leonisWPTheme/js/addons.js
+++ leonisWPTheme/js/addons.js
@@ -581,10 +581,11 @@ $('.button-team').click(function(e) {
     $(this.parentElement.parentElement).css({"transform": "translate(" + hPositionStr + ", " + vPositionStr + ")"});
     $(this.parentElement.parentElement.children[3]).css({"display" : "none"});
     $(this.parentElement.parentElement.children[4]).css({"position": "absolute", "display" : "block", "left": "380px", "top": "10%", "font-size" : "3em"});
-    $(this.parentElement.parentElement.children[4].children[0]).css({"display" : "block"}); // paragraph
+    $(this.parentElement.parentElement.children[4].children[1]).css({"display" : "block"}); // paragraph
     $(this.parentElement.parentElement.children[5]).css({"position": "absolute", "display" : "block", "left": "610px", "top": "10%", "font-size" : "3em"});
+    $(this.parentElement.parentElement.children[5].children[1]).css({"display" : "block"}); // paragraph
     $(this.parentElement.parentElement.children[6]).css({"position": "absolute", "display" : "block", "left": "300px", "top": "10%", "font-size" : "3em"});
-    $(this.parentElement.parentElement.children[6].children[0]).css({"display" : "block"}); // paragraph
+    
 });
 
 
