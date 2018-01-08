44299dc075179d8dd033f77cf5d5cb33d6605517
James Allington-Kay <lancaster.1@hotmail.co.uk>
2018-01-08 21:30:52 +0000

    Bug fix

--- leonisWPTheme/functions.php
+++ leonisWPTheme/functions.php
@@ -4,9 +4,7 @@
 add_filter('wp_mail_from', 'new_mail_from');
 add_filter('wp_mail_from_name', 'new_mail_from_name');
  
-function new_mail_from($old) {
-return 'enquiries@leonispartners.com';
-}
+
 function new_mail_from_name($old) {
 return 'Leonis Partners';
 }
