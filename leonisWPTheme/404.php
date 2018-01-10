23d66ae7e6d9a69a1fb24eb75af4a09b4644fa92
James Allington-Kay <lancaster.1@hotmail.co.uk>
2018-01-10 16:52:31 +0000

    Adding gif

--- leonisWPTheme/404.php
+++ leonisWPTheme/404.php
@@ -8,6 +8,7 @@
 				</div>
 				<div id="page_content" style="width: 75%; left: 12.5%; position: relative;">
 					<h2>We couldn't find the page you are looking for, sorry.</h2>
+					<img class="404" style="padding-top: 200px; width: 70%; height: auto; position: relative; left:50%; transform: translateX(-50%);" src="<?php echo get_bloginfo('template_url') ?>/images/404.gif"/>
 				</div>
 			</div>
 		</div>
